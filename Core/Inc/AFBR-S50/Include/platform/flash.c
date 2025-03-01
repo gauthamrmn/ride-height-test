701848fa0aa0f9039a8f9dec1046da029e88bbc5
Gautham <gauthamr7@outlook.com>
2025-02-28 23:48:28 -0600

    trying to get flash.c to compile with no errors

---
+++ Core/Inc/AFBR-S50/Include/platform/flash.c
@@ -0,0 +1,3 @@
+//
+// Created by Gautham Ramanarayanan on 2/28/25.
+//
---
+++ Core/Inc/AFBR-S50/Include/platform/flash.h
@@ -0,0 +1,8 @@
+//
+// Created by Gautham Ramanarayanan on 2/28/25.
+//
+
+#ifndef FLASH_H
+#define FLASH_H
+
+#endif //FLASH_H
