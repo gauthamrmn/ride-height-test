41b33358d9ea2733cb0186db556bbf3b7f707b79
KaitlynWChang <kaitlynchangnyc@gmail.com>
2025-03-02 16:24:50 -0600

    Added function to store timer counter value in seconds and microseconds

--- Core/Inc/AFBR-S50/Include/platform/argus_timer.c
+++ Core/Inc/AFBR-S50/Include/platform/argus_timer.c
@@ -17,7 +17,29 @@ void Timer_Init(void) {
 	HAL_TIM_Base_Start(&htim5);
 }
 
+/*!***************************************************************************
+ * @brief Obtains the lifetime counter value from the timers.
+ *
+ * @details The function is required to get the current time relative to any
+ * point in time, e.g. the startup time. The returned values \p hct and
+ * \p lct are given in seconds and microseconds respectively. The current
+ * elapsed time since the reference time is then calculated from:
+ *
+ * t_now [µsec] = hct * 1000000 µsec + lct * 1 µsec
+ *
+ * @param hct A pointer to the high counter value bits representing current
+ * time in seconds.
+ * @param lct A pointer to the low counter value bits representing current
+ * time in microseconds. Range: 0, .., 999999 µsec
+ * @return -
+ *****************************************************************************/
 void Timer_GetCounterValue(uint32_t *hct, uint32_t *lct) {
+	/* The loop makes sure that there are no glitches
+	when the counter wraps between htim2 and htm2 reads. */
+	do {
+		*lct = __HAL_TIM_GET_COUNTER(&htim2);
+		*hct = __HAL_TIM_GET_COUNTER(&htim5);
+	} while (*lct > __HAL_TIM_GET_COUNTER(&htim2));
 }
 
 status_t Timer_SetCallback(timer_cb_t f) {
