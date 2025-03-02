b18e8d57272cdf59c07edf894788c5599af53bc0
KaitlynWChang <kaitlynchangnyc@gmail.com>
2025-03-02 17:01:18 -0600

    Added functions to set callback interval and stop the timer

--- Core/Inc/AFBR-S50/Include/platform/argus_timer.c
+++ Core/Inc/AFBR-S50/Include/platform/argus_timer.c
@@ -45,8 +45,6 @@ void Timer_GetCounterValue(uint32_t *hct, uint32_t *lct) {
 status_t Timer_SetCallback(timer_cb_t f) {
 }
 
-status_t Timer_SetInterval(uint32_t dt_microseconds, void *param) {
-}
 
 /*! Storage for the callback parameter */
 static void *callback_param_;
@@ -83,5 +81,33 @@ status_t Timer_Start(uint32_t period, void *param) {
 	return STATUS_OK;
 }
 
-void Timer_Stop(void *param) {
+/*!***************************************************************************
+ * @brief Stops the timer for a specified callback parameter.
+ * @details Stops a callback interval for the specified parameter.
+ * @param param An abstract parameter that identifies the interval to be stopped.
+ * @return Returns the \link #status_t status\endlink (#STATUS_OK on success).
+ *****************************************************************************/
+status_t Timer_Stop(void *param) {
+	period_us_ = 0;
+	callback_param_ = 0;
+	/* Disable interrupt and timer */
+	__HAL_TIM_DISABLE_IT(&htim4, TIM_IT_UPDATE);
+	__HAL_TIM_DISABLE(&htim4);;
+	return STATUS_OK;
+}
+
+/*!***************************************************************************
+ * @brief Sets the timer interval for a specified callback parameter.
+ * @details Sets the callback interval for the specified parameter and starts
+ * the timer with a new interval. If there is already an interval with
+ * the given parameter, the timer is restarted with the given interval.
+ * If the same time interval as already set is passed, nothing happens.
+ * Passing a interval of 0 disables the timer.
+ * @param dt_microseconds The callback interval in microseconds.
+ * @param param An abstract parameter to be passed to the callback. This is
+ * also the identifier of the given interval.
+ * @return Returns the \link #status_t status\endlink (#STATUS_OK on success).
+ *****************************************************************************/
+status_t Timer_SetInterval(uint32_t dt_microseconds, void *param) {
+	return dt_microseconds ? Timer_Start(dt_microseconds, param) : Timer_Stop(param);
 }
