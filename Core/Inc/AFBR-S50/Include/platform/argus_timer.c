353d0c19d4d16458aec1c16185965116c6677bc8
KaitlynWChang <kaitlynchangnyc@gmail.com>
2025-03-02 16:23:55 -0600

    Added timer initialization

--- Core/Inc/AFBR-S50/Include/platform/argus_timer.c
+++ Core/Inc/AFBR-S50/Include/platform/argus_timer.c
@@ -1,18 +1,33 @@
 #include "argus_timer.h"
+#include "tim.h"
 
 #include <stdint.h>
 
-void Timer_GetCounterValue(uint32_t * hct, uint32_t * lct)
-{ }
+/*!***************************************************************************
+ * @brief Initializes the timer hardware.
+ * @return -
+ *****************************************************************************/
+void Timer_Init(void) {
+	/* Initialize the timers, see generated main.c */
+	MX_TIM2_Init();
+	MX_TIM4_Init();
+	MX_TIM5_Init();
+	/* Start the timers relevant for the LTC */
+	HAL_TIM_Base_Start(&htim2);
+	HAL_TIM_Base_Start(&htim5);
+}
 
-status_t Timer_SetCallback(timer_cb_t f)
-{ }
+void Timer_GetCounterValue(uint32_t *hct, uint32_t *lct) {
+}
 
-status_t Timer_SetInterval(uint32_t dt_microseconds, void* param)
-{ }
+status_t Timer_SetCallback(timer_cb_t f) {
+}
 
-void Timer_Start(uint32_t dt_microseconds, void * param)
-{ }
+status_t Timer_SetInterval(uint32_t dt_microseconds, void *param) {
+}
 
-void Timer_Stop(void * param)
-{ }
+void Timer_Start(uint32_t dt_microseconds, void *param) {
+}
+
+void Timer_Stop(void *param) {
+}
